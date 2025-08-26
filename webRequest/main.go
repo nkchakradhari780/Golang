82b877b85ad0ae11889d8053b52fa177338dba68
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2025-08-26 20:09:19 +0530

    fetching data from the api

--- webRequest/main.go
+++ webRequest/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
+
 
 func main() {
 	fmt.Println("Web Request")
+	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer res.Body.Close()
+	fmt.Printf("Type of response: %T\n",res)
+	// fmt.Println("Response: ",res)
+
+	data,err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Response:", string(data))
 }
+ 

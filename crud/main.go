c1cec724d28d468d0a7a2c569b3855517f45ef18
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2025-08-27 10:18:55 +0530

    updated operation performed

--- crud/main.go
+++ crud/main.go
@@ -86,8 +86,49 @@ func performPostRequest() {
 
 }
 
+func performPutRequest() {
+	todo := Todo{
+		UserId:    4546,
+		Title:     "Updated Title",
+		Completed: false,
+	}
+
+	//convert the Todo to JSON
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
+
+	jsonString := string(jsonData)
+
+	//convert string data to reader
+	jsonReader := strings.NewReader(jsonString)
+
+	myUrl := "https://jsonplaceholder.typicode.com/todos/1"
+	req, err := http.NewRequest(http.MethodPut, myUrl, jsonReader)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer res.Body.Close()
+
+	data, _ := io.ReadAll(res.Body)
+	fmt.Println("Response: ", string(data))
+	fmt.Println("Response Status", res.Status)
+}
+
 func main() {
 	fmt.Println("Learning CRUD....")
 	// performGetRequest()
-	performPostRequest()
+	// performPostRequest()
+	performPutRequest()
 }

b34ee376b5b4b7c15fd69088360542ddd64d9585
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2025-08-27 10:22:14 +0530

    delete request performed

--- crud/main.go
+++ crud/main.go
@@ -126,9 +126,31 @@ func performPutRequest() {
 	fmt.Println("Response Status", res.Status)
 }
 
+func performDeleteRequest() {
+	myUrl := "https://jsonplaceholder.typicode.com/todos/1"
+	req, err := http.NewRequest(http.MethodDelete, myUrl, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer res.Body.Close()
+
+	data, _ := io.ReadAll(res.Body)
+	fmt.Println("Response: ", string(data))
+	fmt.Println("Response Status", res.Status)
+}
+
 func main() {
 	fmt.Println("Learning CRUD....")
 	// performGetRequest()
 	// performPostRequest()
-	performPutRequest()
+	// performPutRequest()
+	performDeleteRequest()
 }
